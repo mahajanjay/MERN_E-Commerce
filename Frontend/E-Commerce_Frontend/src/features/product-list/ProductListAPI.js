793c7a93b9bea19e56e81f626e70b2b07a73ebe9
Jayant Mahajan <jayantmahajan99@gmail.com>
2023-08-22 21:29:26 +0530

    product details page with dummy API's

--- Frontend/E-Commerce_Frontend/src/features/product-list/ProductListAPI.js
+++ Frontend/E-Commerce_Frontend/src/features/product-list/ProductListAPI.js
@@ -7,6 +7,14 @@ export function fetchAllProducts() {
   });
 }
 
+export function fetchProductById(id) {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8000/products/"+ id);
+    const data = response.json();
+    resolve({ data });
+  });
+}
+
 export function fetchProductsByFilters(filter, sort, pagination) {
   //filter={"category": ["smartphone", "laptops"]}
   //pagination={_page: 1, _limit=10}
@@ -32,5 +40,21 @@ export function fetchProductsByFilters(filter, sort, pagination) {
     const data = await response.json();
     const totalItems = await response.headers.get('X-Total-Count');
     resolve({ data: {products: data, totalItems:+totalItems} });
+  });
+}
+
+export function fetchCategories() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8000/categories");
+    const data = response.json();
+    resolve({ data });
+  });
+}
+
+export function fetchBrands() {
+  return new Promise(async (resolve) => {
+    const response = await fetch("http://localhost:8000/brands");
+    const data = response.json();
+    resolve({ data });
   });
 }
--- Frontend/E-Commerce_Frontend/src/features/product-list/ProductListSlice.js
+++ Frontend/E-Commerce_Frontend/src/features/product-list/ProductListSlice.js
@@ -1,10 +1,13 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchAllProducts, fetchProductsByFilters } from './ProductListAPI';
+import { fetchAllProducts, fetchBrands, fetchCategories, fetchProductsByFilters, fetchProductById } from './ProductListAPI';
 
 const initialState = {
   products: [],
+  categories: [],
+  brands: [],
   status: 'idle',
-  totalItems: 0
+  totalItems: 0,
+  selectedProduct: null
 };
 
 // The function below is called a thunk and allows us to perform async logic. It
@@ -21,10 +24,37 @@ export const fetchAllProductsAsync = createAsyncThunk(
   }
 );
 
+export const fetchProductByIdAsync = createAsyncThunk(
+  'product/fetchProductById',
+  async (id) => {
+    const response = await fetchProductById(id);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
 export const fetchProductsByFilterAsync = createAsyncThunk(
   'product/fetchProductsByFilters',
   async ({filter, sort, pagination}) => {
     const response = await fetchProductsByFilters(filter, sort, pagination);
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const fetchCategoriesAsync = createAsyncThunk(
+  'product/fetchCategories',
+  async () => {
+    const response = await fetchCategories();
+    // The value we return becomes the `fulfilled` action payload
+    return response.data;
+  }
+);
+
+export const fetchBrandsAsync = createAsyncThunk(
+  'product/fetchBrands',
+  async () => {
+    const response = await fetchBrands();
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
@@ -56,7 +86,28 @@ export const productSlice = createSlice({
         state.status = 'idle';
         state.products = action.payload.products;
         state.totalItems = action.payload.totalItems;
-      });
+      })
+      .addCase(fetchCategoriesAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchCategoriesAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.categories = action.payload;
+      })
+      .addCase(fetchBrandsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchBrandsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.brands = action.payload;
+      })
+      .addCase(fetchProductByIdAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductByIdAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.selectedProduct = action.payload;
+      })
   },
 });
 
@@ -66,6 +117,9 @@ export const { increment } = productSlice.actions;
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectAllProducts = (state) => state.product.products;
+export const selectProductById = (state) => state.product.selectedProduct;
 export const selectTotalItems = (state) => state.product.totalItems;
+export const selectCategories = (state) => state.product.categories;
+export const selectBrands = (state) => state.product.brands;
 
 export default productSlice.reducer;
